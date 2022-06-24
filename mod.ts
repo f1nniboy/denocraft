f3765e58fe669b8097011107c5eb4dc104a82e37
f1nniboy <f1nniboy.me@gmail.com>
2022-06-24 20:04:20 +0200

    Export item & slot classes

--- mod.ts
+++ mod.ts
@@ -1,7 +1,13 @@
-/* Block & Item */
+/* Block */
 export { Block, BlockUpdateCause } from "./lib/block/block.ts";
 export { Identifier } from "./lib/block/identifier.ts";
 export type { BlockUpdateEvent } from "./lib/block/block.ts";
+
+/* Item & Slot */
+export { SlotReference } from "./lib/inventory/slot/reference.ts";
+export { ItemSlot } from "./lib/inventory/slot/item.ts";
+export { Slot } from "./lib/inventory/slot/slot.ts";
+export { Item } from "./lib/inventory/item.ts";
 
 /* World */
 export { Location, WorldLocation } from "./lib/world/location.ts";
